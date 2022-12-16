8b25d04c293550b4666dc0f79c0cf25e8ceca685
candiduslynx <candiduslynx@gmail.com>
2022-12-16 17:24:17 +0200

    sentry dsn

--- plugins/source/snyk/main.go
+++ plugins/source/snyk/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
-const sentryDSN = "TODO"
+const sentryDSN = "https://c2101fe3c5fd4f91a095b2b37dc6364a@o1396617.ingest.sentry.io/4504333793165313"
 
 func main() {
 	serve.Source(plugin.Snyk(), serve.WithSourceSentryDSN(sentryDSN))
